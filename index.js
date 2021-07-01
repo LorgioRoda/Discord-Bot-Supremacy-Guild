f857bd78ff8e97e3872012036a7c68936823632b
Lorgio Roda <lorgiorodaroca@gmail.com>
2021-07-01 17:24:53 -0400

    begin project

--- index.js
+++
@@ -1,8 +0,0 @@
-const Discord = require('discord.js');
-const Client = new Discord.Client();
-
-Client.on('message', () => {
-    console.log('Client is ready')
-});
-
-Client.login(process.env.TOKEN);
